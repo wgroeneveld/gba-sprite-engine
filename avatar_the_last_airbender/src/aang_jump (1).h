f2a5a4388f0046fe3de7886db0eefa6ba43f3d82
WouterVanRompaey <47601039+WouterVanRompaey@users.noreply.github.com>
2019-12-05 14:59:27 +0100

    aang jumping sprite added

---
+++ avatar_the_last_airbender/src/aang_jump (1).h
@@ -0,0 +1,33 @@
+//
+// Created by woute on 5/12/2019.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_AANG_JUMP_H
+#define GBA_SPRITE_ENGINE_PROJECT_AANG_JUMP_H
+//{{BLOCK(aang_jump__1_)
+
+//======================================================================
+//
+//	aang_jump__1_, 48x376@8, 
+//	+ palette 256 entries, not compressed
+//	+ 282 tiles not compressed
+//	Total size: 512 + 18048 = 18560
+//
+//	Time-stamp: 2019-12-05, 14:39:10
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_AANG_JUMP__1__H
+#define GRIT_AANG_JUMP__1__H
+
+#define aang_jump__1_TilesLen 18048
+extern const unsigned short aang_jump__1_Tiles[9024];
+
+#define aang_jump__1_PalLen 512
+extern const unsigned short aang_jump__1_Pal[256];
+
+#endif // GRIT_AANG_JUMP__1__H
+
+//}}BLOCK(aang_jump__1_)
