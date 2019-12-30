2733820a9f7cd686cf13d5553af5aa2684013cd0
JanVdBerckt <jan.vandenberckt@student.uhasselt.be>
2019-12-30 23:10:20 +0100

    Extra klasses

---
+++ Metroid/src/Bullet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by SDM Technics on 30/12/2019.
+//
+
+#include "Bullet.h"
--- Metroid/src/Metroid.cpp
+++ Metroid/src/Metroid.cpp
@@ -7,25 +7,34 @@
 #include <libgba-sprite-engine/gba_engine.h>
 #include <libgba-sprite-engine/gba/tonc_math.h>
 
+int Metroid::getLives() {
+    return lives;
+}
+
+void Metroid::reduceLives(int value) {
+    lives -= value;
+}
+
 void Metroid::tick(u16 keys) {
-    getMetroid()->stopAnimating();
+
     if(keys & KEY_LEFT) {
-        getMetroid()->animate();
+        getMetroid()->makeAnimated(6,5);
         getMetroid()->flipHorizontally(true);
         getMetroid()->setVelocity(-2, 0);
     } else if(keys & KEY_RIGHT) {
         if(getMetroid()->getX()>=158){
-            getMetroid()->animate();
+            getMetroid()->makeAnimated(6,5);
             getMetroid()->flipHorizontally(false);
             getMetroid()->setVelocity(0, 0);
         }
         else{
-            getMetroid()->animate();
+            getMetroid()->makeAnimated(6,5);
             getMetroid()->flipHorizontally(false);
             getMetroid()->setVelocity(+2, 0);
         }
     } else {
-        getMetroid()->animateToFrame(0);
+        getMetroid()->animateToFrame(7);
+        getMetroid()->stopAnimating();
         getMetroid()->setVelocity(0, 0);
     }
 }
