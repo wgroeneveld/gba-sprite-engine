76304eecb1d7d0ec0718ee0e5788af52dd8127c3
Anouk Michiels <anouk.michiels@student.uhasselt.be>
2020-04-12 20:04:50 +0200

    forgot to add files to git

---
+++ partyGame/img/basketballAnimated.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(basketballAnimated)
+
+//======================================================================
+//
+//	basketballAnimated, 32x128@8, 
+//	+ palette 256 entries, not compressed
+//	+ 64 tiles Metatiled by 4x4 not compressed
+//	Total size: 512 + 4096 = 4608
+//
+//	Time-stamp: 2020-04-12, 19:27:21
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BASKETBALLANIMATED_H
+#define GRIT_BASKETBALLANIMATED_H
+
+#define basketballAnimatedTilesLen 4096
+extern const unsigned int basketballAnimatedTiles[1024];
+
+#define basketballAnimatedPalLen 512
+extern const unsigned short basketballAnimatedPal[256];
+
+#endif // GRIT_BASKETBALLANIMATED_H
+
+//}}BLOCK(basketballAnimated)
---
+++ partyGame/img/stickmanAnimated.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(stickmanAnimated)
+
+//======================================================================
+//
+//	stickmanAnimated, 32x128@8, 
+//	+ palette 256 entries, not compressed
+//	+ 64 tiles Metatiled by 4x4 not compressed
+//	Total size: 512 + 4096 = 4608
+//
+//	Time-stamp: 2020-04-12, 19:42:47
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_STICKMANANIMATED_H
+#define GRIT_STICKMANANIMATED_H
+
+#define stickmanAnimatedTilesLen 4096
+extern const unsigned int stickmanAnimatedTiles[1024];
+
+#define stickmanAnimatedPalLen 512
+extern const unsigned short stickmanAnimatedPal[256];
+
+#endif // GRIT_STICKMANANIMATED_H
+
+//}}BLOCK(stickmanAnimated)
