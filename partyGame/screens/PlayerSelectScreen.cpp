7c1899812beae937e5a079dddf80731e14560ace
Anouk Michiels <anouk.michiels@student.uhasselt.be>
2020-08-10 15:42:42 +0200

    Added SelectPlayerScreen

--- partyGame/screens/PlayerSelectScreen.cpp
+++ partyGame/screens/PlayerSelectScreen.cpp
@@ -1,6 +1,9 @@
 //
-// Created by michi on 21/03/2020.
+// Created by michi on 10/08/2020.
 //
+
+#include "PlayerSelectScreen.h"
+
 
 #include <libgba-sprite-engine/gba_engine.h>
 #include <libgba-sprite-engine/background/text_stream.h>
@@ -8,24 +11,20 @@
 #include <libgba-sprite-engine/effects/fade_out_scene.h>
 #include <libgba-sprite-engine/sprites/sprite_builder.h>
 
-#include "MainMenuScreen.h"
-#include "AboutScreen.h"
 #include "GameScreen.h"
-#include "PlayerSelectScreen.h"
 
 #include "../backgrounds/MainMenu/pijl.h"
 #include "../backgrounds/gras.h"
 #include "../backgrounds/grasBackground.h"
 
-std::vector<Background *> MainMenuScreen::backgrounds() {return {background.get()};}
+std::vector<Background *> PlayerSelectScreen::backgrounds() {return {/*background.get()*/};}
 
-std::vector<Sprite *> MainMenuScreen::sprites() {return {pijl.get()};}
+std::vector<Sprite *> PlayerSelectScreen::sprites() {return{}/* {pijl.get(), spook1.get(), spook1.get()}*/;}
 
-void MainMenuScreen::load() {
-    TextStream::instance().setText(std::string("Start Game"), 7, 5);
-    TextStream::instance().setText(std::string("Read more about the game"), 10, 5);
-    TextStream::instance().setText(std::string("Welcome to Ghost Town"), 2, 5);
+void PlayerSelectScreen::load() {
+    TextStream::instance().setText(std::string("Choose Your player"), 2, 5);
 
+    /*
     foregroundPalette = std::unique_ptr<ForegroundPaletteManager>(new ForegroundPaletteManager(pijlPal, sizeof(pijlPal)));
     SpriteBuilder<Sprite> spriteBuilder;
     pijl = spriteBuilder
@@ -37,42 +36,42 @@ void MainMenuScreen::load() {
 
     backgroundPalette = std::unique_ptr<BackgroundPaletteManager>(new BackgroundPaletteManager(grasPal, sizeof(grasPal)));
     background = std::unique_ptr<Background>(new Background(1, grasTiles, sizeof(grasTiles), grasBackground, sizeof(grasBackground)));
-    background->useMapScreenBlock(16);
+    background->useMapScreenBlock(16);*/
 }
 
-void MainMenuScreen::tick(u16 keys) {
+void PlayerSelectScreen::tick(u16 keys) {
 
     if (firstTick) {
         firstTick = false;
         lastKeys = keys;
         return;
-    }
+    }/*
 
     if (!(keys & KEY_START) && (lastKeys & KEY_START)) {// ENTER key, wait until released
-        if (keuze == 0) {
+        if (spelerKeuze == 0) {
             if (!engine->isTransitioning()) {
-                //engine->transitionIntoScene(new GameScreen(engine), new FadeOutScene(2));
-                engine->transitionIntoScene(new PlayerSelectScreen(engine), new FadeOutScene(2));
+                engine->transitionIntoScene(new GameScreen(engine), new FadeOutScene(2));
             }
         }
         else {
-            engine->transitionIntoScene(new AboutScreen(engine), new FadeOutScene(2));
+            engine->transitionIntoScene(new GameScreen(engine), new FadeOutScene(2));
         }
     }
     else if ((keys & KEY_DOWN) && !(lastKeys & KEY_DOWN)) {
-        if (keuze == 0) {
-            keuze++;
+        if (spelerKeuze == 0) {
+            spelerKeuze++;
             updatePijl();
         }
     }
     else if ((keys & KEY_UP) && !(lastKeys & KEY_UP)) {
-        if (keuze == 1) {
-            keuze--;
+        if (spelerKeuze == 1) {
+            spelerKeuze--;
             updatePijl();
         }
     }
-    lastKeys = keys;
+    lastKeys = keys;*/
 }
-void MainMenuScreen::updatePijl() {
-    pijl->moveTo(5, 46 + keuze * 24);
-}
+/*
+void PlayerSelectScreen::updatePijl() {
+    pijl->moveTo(5, 46 + spelerKeuze * 24);
+}*/
---
+++ partyGame/screens/PlayerSelectScreen.h
@@ -0,0 +1,42 @@
+//
+// Created by michi on 10/08/2020.
+//
+
+#ifndef GBA_SPRITE_ENGINE_PROJECT_PLAYERSELECTSCREEN_H
+#define GBA_SPRITE_ENGINE_PROJECT_PLAYERSELECTSCREEN_H
+
+
+#include <memory>
+#include <libgba-sprite-engine/sprites/sprite.h>
+#include <libgba-sprite-engine/background/background.h>
+#include <libgba-sprite-engine/scene.h>
+
+class PlayerSelectScreen : public Scene {
+
+private:
+    std::unique_ptr<Sprite> pijl;
+    std::unique_ptr<Sprite> spook1;
+    std::unique_ptr<Sprite> spook2;
+
+    std::unique_ptr<Background> background;
+    int spelerKeuze = 0;
+
+    u16 lastKeys = 0;
+    bool firstTick = true;
+
+public:
+    std::vector<Sprite *> sprites() override;
+    std::vector<Background *> backgrounds() override;
+
+    PlayerSelectScreen(std::shared_ptr<GBAEngine> engine) : Scene(engine) {};
+    //~MainMenuScreen(){};
+
+
+    void load() override;
+    void tick(u16 keys) override;
+    void updatePijl();
+
+};
+
+
+#endif //GBA_SPRITE_ENGINE_PROJECT_PLAYERSELECTSCREEN_H
