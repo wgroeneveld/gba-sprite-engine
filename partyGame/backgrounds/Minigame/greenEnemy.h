4455717fa777242e1d63e425157b6b0d9a510b71
Anouk Michiels <anouk.michiels@student.uhasselt.be>
2020-08-11 15:01:16 +0200

    Added files

---
+++ partyGame/backgrounds/Minigame/greenEnemy.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(greenEnemyCenter)
+
+//======================================================================
+//
+//	greenEnemyCenter, 32x32@8, 
+//	+ 16 tiles Metatiled by 4x4 not compressed
+//	Total size: 1024 = 1024
+//
+//	Time-stamp: 2020-08-11, 14:08:26
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_GREENENEMYCENTER_H
+#define GRIT_GREENENEMYCENTER_H
+
+#define greenEnemyCenterTilesLen 1024
+extern const unsigned int greenEnemyCenterTiles[256];
+
+#endif // GRIT_GREENENEMYCENTER_H
+
+//}}BLOCK(greenEnemyCenter)
---
+++ partyGame/backgrounds/Minigame/redEnemy.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(redEnemyCenter)
+
+//======================================================================
+//
+//	redEnemyCenter, 32x32@8, 
+//	+ 16 tiles Metatiled by 4x4 not compressed
+//	Total size: 1024 = 1024
+//
+//	Time-stamp: 2020-08-11, 14:08:26
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_REDENEMYCENTER_H
+#define GRIT_REDENEMYCENTER_H
+
+#define redEnemyCenterTilesLen 1024
+extern const unsigned int redEnemyCenterTiles[256];
+
+#endif // GRIT_REDENEMYCENTER_H
+
+//}}BLOCK(redEnemyCenter)
